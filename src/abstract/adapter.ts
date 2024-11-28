0481032bb8ba8b2a245730798f5d6ff56268e889
CarlosHenrique-dd <carlos.alves.ca12@gmail.com>
2024-11-28 09:59:45 -0300

    fix: if desnecessario

--- src/abstract/adapter.ts
+++ src/abstract/adapter.ts
@@ -1,5 +1,4 @@
 export const adapt = (controller) => async (req, res) => {
     const { status, data } = await controller.handle(req)
-    if (status !== 200) return res.status(status).send(data)
     return res.status(status).send(data)
 }
